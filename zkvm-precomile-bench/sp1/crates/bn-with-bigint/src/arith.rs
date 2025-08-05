501eaed4c43e7ea0e32132d01f4c890a74d4fb75
developeruche <developeruche@gmail.com>
2025-08-05 09:30:56 +0100

    adapted arith module

--- zkvm-precomile-bench/sp1/crates/bn-with-bigint/src/arith.rs
+++ zkvm-precomile-bench/sp1/crates/bn-with-bigint/src/arith.rs
@@ -1,195 +1,7 @@
-use core::cmp::Ordering;
-use rand::Rng;
+use alloc::vec::Vec;
+use crypto_bigint::U256;
 use crunchy::unroll;
 
-use byteorder::{BigEndian, ByteOrder};
-
-/// 256-bit, stack allocated biginteger for use in prime field
-/// arithmetic.
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-#[repr(C)]
-pub struct U256(pub [u128; 2]);
-
-impl From<[u64; 4]> for U256 {
-    fn from(d: [u64; 4]) -> Self {
-        let mut a = [0u128; 2];
-        a[0] = (d[1] as u128) << 64 | d[0] as u128;
-        a[1] = (d[3] as u128) << 64 | d[2] as u128;
-        U256(a)
-    }
-}
-
-impl From<u64> for U256 {
-    fn from(d: u64) -> Self {
-        U256::from([d, 0, 0, 0])
-    }
-}
-
-/// 512-bit, stack allocated biginteger for use in extension
-/// field serialization and scalar interpretation.
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-#[repr(C)]
-pub struct U512(pub [u128; 4]);
-
-impl From<[u64; 8]> for U512 {
-    fn from(d: [u64; 8]) -> Self {
-        let mut a = [0u128; 4];
-        a[0] = (d[1] as u128) << 64 | d[0] as u128;
-        a[1] = (d[3] as u128) << 64 | d[2] as u128;
-        a[2] = (d[5] as u128) << 64 | d[4] as u128;
-        a[3] = (d[7] as u128) << 64 | d[6] as u128;
-        U512(a)
-    }
-}
-
-impl U512 {
-    /// Multiplies c1 by modulo, adds c0.
-    pub fn new(c1: &U256, c0: &U256, modulo: &U256) -> U512 {
-        let mut res = [0; 4];
-
-        debug_assert_eq!(c1.0.len(), 2);
-        unroll! {
-            for i in 0..2 {
-                mac_digit(i, &mut res, &modulo.0, c1.0[i]);
-            }
-        }
-
-        let mut carry = 0;
-
-        debug_assert_eq!(res.len(), 4);
-        unroll! {
-            for i in 0..2 {
-                res[i] = adc(res[i], c0.0[i], &mut carry);
-            }
-        }
-
-        unroll! {
-            for i in 0..2 {
-                let (a1, a0) = split_u128(res[i + 2]);
-                let (c, r0) = split_u128(a0 + carry);
-                let (c, r1) = split_u128(a1 + c);
-                carry = c;
-
-                res[i + 2] = combine_u128(r1, r0);
-            }
-        }
-
-        debug_assert!(0 == carry);
-
-        U512(res)
-    }
-
-     pub fn from_slice(s: &[u8]) -> Result<U512, Error> {
-        if s.len() != 64 {
-            return Err(Error::InvalidLength {
-                expected: 32,
-                actual: s.len(),
-            });
-        }
-
-        let mut n = [0; 4];
-        for (l, i) in (0..4).rev().zip((0..4).map(|i| i * 16)) {
-            n[l] = BigEndian::read_u128(&s[i..]);
-        }
-
-        Ok(U512(n))
-    }
-
-    /// Get a random U512
-    pub fn random<R: Rng>(rng: &mut R) -> U512 {
-        U512(rng.gen())
-    }
-
-    pub fn get_bit(&self, n: usize) -> Option<bool> {
-        if n >= 512 {
-            None
-        } else {
-            let part = n / 128;
-            let bit = n - (128 * part);
-
-            Some(self.0[part] & (1 << bit) > 0)
-        }
-    }
-
-    /// Divides self by modulo, returning remainder and, if
-    /// possible, a quotient smaller than the modulus.
-    pub fn divrem(&self, modulo: &U256) -> (Option<U256>, U256) {
-        let mut q = Some(U256::zero());
-        let mut r = U256::zero();
-
-        for i in (0..512).rev() {
-            // NB: modulo's first two bits are always unset
-            // so this will never destroy information
-            mul2(&mut r.0);
-            assert!(r.set_bit(0, self.get_bit(i).unwrap()));
-            if &r >= modulo {
-                sub_noborrow(&mut r.0, &modulo.0);
-                if q.is_some() && !q.as_mut().unwrap().set_bit(i, true) {
-                    q = None
-                }
-            }
-        }
-
-        if q.is_some() && (q.as_ref().unwrap() >= modulo) {
-            (None, r)
-        } else {
-            (q, r)
-        }
-    }
-
-    pub fn interpret(buf: &[u8; 64]) -> U512 {
-        let mut n = [0; 4];
-        for (l, i) in (0..4).rev().zip((0..4).map(|i| i * 16)) {
-            n[l] = BigEndian::read_u128(&buf[i..]);
-        }
-
-        U512(n)
-    }
-}
-
-impl Ord for U512 {
-    #[inline]
-    fn cmp(&self, other: &U512) -> Ordering {
-        for (a, b) in self.0.iter().zip(other.0.iter()).rev() {
-            if *a < *b {
-                return Ordering::Less;
-            } else if *a > *b {
-                return Ordering::Greater;
-            }
-        }
-
-        return Ordering::Equal;
-    }
-}
-
-impl PartialOrd for U512 {
-    #[inline]
-    fn partial_cmp(&self, other: &U512) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for U256 {
-    #[inline]
-    fn cmp(&self, other: &U256) -> Ordering {
-        for (a, b) in self.0.iter().zip(other.0.iter()).rev() {
-            if *a < *b {
-                return Ordering::Less;
-            } else if *a > *b {
-                return Ordering::Greater;
-            }
-        }
-
-        return Ordering::Equal;
-    }
-}
-
-impl PartialOrd for U256 {
-    #[inline]
-    fn partial_cmp(&self, other: &U256) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
 
 /// U256/U512 errors
 #[derive(Debug)]
@@ -197,184 +9,7 @@ pub enum Error {
     InvalidLength { expected: usize, actual: usize },
 }
 
-impl U256 {
-    /// Initialize U256 from slice of bytes (big endian)
-    pub fn from_slice(s: &[u8]) -> Result<U256, Error> {
-        if s.len() != 32 {
-            return Err(Error::InvalidLength {
-                expected: 32,
-                actual: s.len(),
-            });
-        }
 
-        let mut n = [0; 2];
-        for (l, i) in (0..2).rev().zip((0..2).map(|i| i * 16)) {
-            n[l] = BigEndian::read_u128(&s[i..]);
-        }
-
-        Ok(U256(n))
-    }
-
-    pub fn to_big_endian(&self, s: &mut [u8]) -> Result<(), Error> {
-        if s.len() != 32 {
-            return Err(Error::InvalidLength {
-                expected: 32,
-                actual: s.len(),
-            });
-        }
-
-        for (l, i) in (0..2).rev().zip((0..2).map(|i| i * 16)) {
-            BigEndian::write_u128(&mut s[i..], self.0[l]);
-        }
-
-        Ok(())
-    }
-
-    #[inline]
-    pub fn zero() -> U256 {
-        U256([0, 0])
-    }
-
-    #[inline]
-    pub fn one() -> U256 {
-        U256([1, 0])
-    }
-
-    /// Produce a random number (mod `modulo`)
-    pub fn random<R: Rng>(rng: &mut R, modulo: &U256) -> U256 {
-        U512::random(rng).divrem(modulo).1
-    }
-
-    pub fn is_zero(&self) -> bool {
-        self.0[0] == 0 && self.0[1] == 0
-    }
-
-    pub fn set_bit(&mut self, n: usize, to: bool) -> bool {
-        if n >= 256 {
-            false
-        } else {
-            let part = n / 128;
-            let bit = n - (128 * part);
-
-            if to {
-                self.0[part] |= 1 << bit;
-            } else {
-                self.0[part] &= !(1 << bit);
-            }
-
-            true
-        }
-    }
-
-    pub fn get_bit(&self, n: usize) -> Option<bool> {
-        if n >= 256 {
-            None
-        } else {
-            let part = n / 128;
-            let bit = n - (128 * part);
-
-            Some(self.0[part] & (1 << bit) > 0)
-        }
-    }
-
-    /// Add `other` to `self` (mod `modulo`)
-    pub fn add(&mut self, other: &U256, modulo: &U256) {
-        add_nocarry(&mut self.0, &other.0);
-
-        if *self >= *modulo {
-            sub_noborrow(&mut self.0, &modulo.0);
-        }
-    }
-
-    /// Subtract `other` from `self` (mod `modulo`)
-    pub fn sub(&mut self, other: &U256, modulo: &U256) {
-        if *self < *other {
-            add_nocarry(&mut self.0, &modulo.0);
-        }
-
-        sub_noborrow(&mut self.0, &other.0);
-    }
-
-    /// Multiply `self` by `other` (mod `modulo`) via the Montgomery
-    /// multiplication method.
-    pub fn mul(&mut self, other: &U256, modulo: &U256, inv: u128) {
-        mul_reduce(&mut self.0, &other.0, &modulo.0, inv);
-
-        if *self >= *modulo {
-            sub_noborrow(&mut self.0, &modulo.0);
-        }
-    }
-
-    /// Turn `self` into its additive inverse (mod `modulo`)
-    pub fn neg(&mut self, modulo: &U256) {
-        if *self > Self::zero() {
-            let mut tmp = modulo.0;
-            sub_noborrow(&mut tmp, &self.0);
-
-            self.0 = tmp;
-        }
-    }
-
-    #[inline]
-    pub fn is_even(&self) -> bool {
-        self.0[0] & 1 == 0
-    }
-
-    /// Turn `self` into its multiplicative inverse (mod `modulo`)
-    pub fn invert(&mut self, modulo: &U256) {
-        // Guajardo Kumar Paar Pelzl
-        // Efficient Software-Implementation of Finite Fields with Applications to Cryptography
-        // Algorithm 16 (BEA for Inversion in Fp)
-
-        let mut u = *self;
-        let mut v = *modulo;
-        let mut b = U256::one();
-        let mut c = U256::zero();
-
-        while u != U256::one() && v != U256::one() {
-            while u.is_even() {
-                div2(&mut u.0);
-
-                if b.is_even() {
-                    div2(&mut b.0);
-                } else {
-                    add_nocarry(&mut b.0, &modulo.0);
-                    div2(&mut b.0);
-                }
-            }
-            while v.is_even() {
-                div2(&mut v.0);
-
-                if c.is_even() {
-                    div2(&mut c.0);
-                } else {
-                    add_nocarry(&mut c.0, &modulo.0);
-                    div2(&mut c.0);
-                }
-            }
-
-            if u >= v {
-                sub_noborrow(&mut u.0, &v.0);
-                b.sub(&c, modulo);
-            } else {
-                sub_noborrow(&mut v.0, &u.0);
-                c.sub(&b, modulo);
-            }
-        }
-
-        if u == U256::one() {
-            self.0 = b.0;
-        } else {
-            self.0 = c.0;
-        }
-    }
-
-    /// Return an Iterator<Item=bool> over all bits from
-    /// MSB to LSB.
-    pub fn bits(&self) -> BitIterator {
-        BitIterator { int: &self, n: 256 }
-    }
-}
 
 pub struct BitIterator<'a> {
     int: &'a U256,
@@ -385,12 +20,13 @@ impl<'a> Iterator for BitIterator<'a> {
     type Item = bool;
 
     fn next(&mut self) -> Option<bool> {
+        let bytes = self.int.to_le_bytes();
+        let bits = bytes_to_bits_le(&bytes);
         if self.n == 0 {
             None
         } else {
             self.n -= 1;
-
-            self.int.get_bit(self.n)
+            Some(bits[self.n])
         }
     }
 }
@@ -542,4 +178,17 @@ fn mul_reduce(this: &mut [u128; 2], by: &[u128; 2], modulus: &[u128; 2], inv: u1
     }
 
     this.copy_from_slice(&res[2..]);
+}
+
+#[inline]
+pub fn bytes_to_bits_le(bytes: &[u8]) -> Vec<bool> {
+    let mut bits = Vec::with_capacity(bytes.len() * 8);
+    for byte in bytes {
+        for i in 0..8 {
+            // Check if the i-th bit is set
+            let bit = (byte >> i) & 1 == 1;
+            bits.push(bit);
+        }
+    }
+    bits
 }
